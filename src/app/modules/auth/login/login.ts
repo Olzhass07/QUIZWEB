c28539bfaf7a9e9a7c14e5ba46dfc56b0337b8a1
Olzhass07 <bolatulyolzhas@gmail.com>
2025-10-28 01:25:12 +0500

    11

--- src/app/modules/auth/login/login.ts
+++ src/app/modules/auth/login/login.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../shared/shared-module';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { Router } from '@angular/router';
+import { Auth } from '../signup/services/auth';
+
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class Login {
+  constructor(private fb: FormBuilder,
+    private auth: Auth,
+    private message: NzMessageService,
+    private router: Router,
+  ) {}
+  validateForm! : FormGroup;
 
+  ngOnInit() {
+  this.validateForm = this.fb.group({
+    email: [null, Validators.required],
+    password: [null, Validators.required]
+  })
+  
+  }
 }
