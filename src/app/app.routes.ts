9d50a5227b7f0d2fa8064b000e706670444eb653
Olzhass07 <bolatulyolzhas@gmail.com>
2025-10-29 15:30:32 +0500

    15

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { Signup } from './modules/auth/signup/signup';
 import { Login } from './modules/auth/login/login';
 
 export const routes: Routes = [
-    {path: 'register', component: Signup},
-    {path: 'login', component: Login}
+  { path: 'register', component: Signup },
+  { path: 'login', component: Login },
+  { path: 'user', loadChildren: () => import('./modules/user/user-module').then(m => m.UserModule) },
+  { path: 'admin', loadChildren: () => import('./modules/admin/admin-module').then(m => m.AdminModule) },
 ];
